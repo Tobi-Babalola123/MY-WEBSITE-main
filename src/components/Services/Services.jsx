d763d62bc913545b21657dc750b06fb582ce489b
Tobi <babalolatobiloba4real@gmail.com>
2024-08-15 17:48:35 +0100

    new

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from "./Tobi's_RESUME.pdf";
+import Resume from "./myresumee.pdf";
 
 const Services = () => {
   // context
